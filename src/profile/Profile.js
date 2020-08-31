ed6a39d25124d36167a20e8ccbadf4eec16f0e0a
aichaabdelali <aicha.abdelali6@gmail.com>
2020-08-31 23:33:14 +0200

    Props Checkpoint 2

---
+++ src/profile/Profile.js
@@ -0,0 +1,53 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Profile = (props) => {
+  Profile.defaultProps = {
+    fullName: "Mr Smith",
+    bio: "bio",
+    profession: "unknown",
+  };
+  Profile.propTypes = {
+    fullName: PropTypes.string.isRequired,
+    bio: PropTypes.string.isRequired,
+    profession: PropTypes.string.isRequired,
+    handleAlert: PropTypes.func.isRequired,
+  };
+  const handleAlert = (fullName) => {
+    alert(`Welcome ${fullName} !`);
+  };
+  return (
+    <div>
+      <h2 style={{ fontFamily: "sans-serif", textAlign: "center" }}> Name : {props.fullName} </h2>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSQxj9ZTZxcHRhi80Y0K_1zFiAEZmsdHxaj2w&usqp=CAU"
+        alt="avatar"
+        width="200px"
+        height="200px"
+        style={{ paddingLeft: "580px" }}
+      />
+      <h4 style={{ fontFamily: "sans-serif", textAlign: "center" }}> Bio : {props.bio} </h4>
+      <h4 style={{ fontFamily: "sans-serif", textAlign: "center" }}>
+        {" "}
+        Profession : {props.profession}
+      </h4>
+      <button
+        onClick={() => handleAlert(props.fullName)}
+        style={{
+          marginLeft: "300px",
+          backgroundColor: "darkblue",
+          color: "white",
+          fontFamily: "sans-serif",
+          width: "120px",
+          height: "50px",
+          borderRadius: "6px",
+          position: "flex-end",
+        }}
+      >
+        Click here !
+      </button>
+    </div>
+  );
+};
+
+export default Profile;
